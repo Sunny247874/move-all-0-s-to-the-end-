d1b388c11e1e41e77dad61f4ef760cf8fe170e58
Ayush Kumar <124272617+Sunny247874@users.noreply.github.com>
2023-06-25 16:59:03 +0530

    Add files via upload

---
+++ In_Place_Replace.java
@@ -0,0 +1,37 @@
+package array_Assignment_1;
+
+import java.util.Arrays;
+
+public class In_Place_Replace {
+
+	
+	public static void moveZero(int arr[])
+	{
+		int n=arr.length;
+		int left=0;
+		int right=0;
+		
+		while(right<n)
+		{
+			if(arr[right]!=0)
+			{
+				int temp=arr[left];
+				arr[left]=arr[right];
+				arr[right]=temp;
+				left++;
+			}
+			right++;
+		}
+		 
+		
+	}
+	public static void main(String[] args) {
+		
+		int arr[]= {0,1,0,3,12};
+		moveZero(arr);
+		System.out.println(Arrays.toString(arr));
+		
+
+	}
+
+}
